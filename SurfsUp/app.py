6eff2249ff8a6bd2e75ac11b1dee50ebd82f5c26
snbadeaux@gmail.com <snbadeaux@gmail.com>
2023-08-23 11:16:19 -0500

    adding changes to app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,5 @@
 # Import all dependencies: 
 ################################################# 
-
 import numpy as np
 
 import sqlalchemy
@@ -13,7 +12,6 @@ from flask import Flask, jsonify
 
 # Create connection to Hawaii.sqlite file
 #################################################
-
 engine = create_engine("sqlite:///Resources\hawaii.sqlite")
 
 # reflect an existing database into a new model
@@ -31,8 +29,6 @@ Station = Base.classes.station
 app = Flask(__name__)
 
 # Create Flask Routes 
-
-# Create root route
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -45,15 +41,13 @@ def welcome():
         f"Temperature stat from start to end dates(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd"        
     )
 
-# Create a route that queries precipiation levels and dates and returns a dictionary using date as key and precipation as value
+# Create precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all daily precipitation totals for the last year"""
-    # Query and summarize daily precipitation across all stations for the last year of available data
-    
+    # Query precipitation for the last year
     start_date = '2016-08-23'
     sel = [Measurement.date, 
         func.sum(Measurement.prcp)]
@@ -76,7 +70,7 @@ def precipitation():
 
     return jsonify(precipitation_dict)
 
-# Create a route that returns a JSON list of stations from the database
+# Create stations route
 @app.route("/api/v1.0/stations")
 def station(): 
 
@@ -94,50 +88,48 @@ def station():
         stations_values.append(stations_values_dict)
     return jsonify (stations_values) 
 
-# Create a route that queries the dates and temp observed for the most active station for the last year of data and returns a JSON list of the temps observed for the last year
+# Create temperature observation route 
 @app.route("/api/v1.0/tobs") 
 def tobs():
     session = Session(engine)
     
     # Create query to find the last date in the database
-    start_date = session.query(Measurement.date).\
+    last_date = session.query(Measurement.date).\
         order_by(Measurement.date.desc()).first() 
 
-    print(start_date)
-    # last_year_date returns row ('2017-08-23',), use this to create a date time object to find start query date 
+    print(last_date)
     
     # check to see if last year was correctly returned by creating dictionary to return last year value to browser in JSON format
     date_values = []
-    for date in start_date:
+    for date in last_date:
         last_year_dict = {}
         last_year_dict["date"] = date
         date_values.append(last_year_dict) 
     print(date_values)
-    # returns: [{'date': '2017-08-23'}]
+   
 
     # Create query_start_date by finding the difference between date time object of "2017-08-23" - 365 days
-    query_start_date = dt.date(2017, 8, 23)-dt.timedelta(days =365) 
-    print(query_start_date) 
+    start_date = dt.date(2017, 8, 23)-dt.timedelta(days =365) 
+    print(start_date) 
     # returns: 2016-08-23 
 
-    # Create query to find most active station in the database 
-    active_station= session.query(Measurement.station, func.count(Measurement.station)).\
+    # Find most active station
+    active_station = session.query(Measurement.station, func.count(Measurement.station)).\
         order_by(func.count(Measurement.station).desc()).\
         group_by(Measurement.station).first()
     most_active_station = active_station[0] 
 
     session.close() 
-     # active_station returns: ('USC00519281', 2772), index to get the first position to isolate most active station number
     print(most_active_station)
     # returns: USC00519281  
 
-    # Create a query to find dates and tobs for the most active station (USC00519281) within the last year (> 2016-08-23)
+    # Find dates and tobs for the most active station (USC00519281) within the last year 
     active_station_measurements = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
-        filter(Measurement.date > query_start_date).\
+        filter(Measurement.date > start_date).\
         filter(Measurement.station == most_active_station) 
     
 
-    # Create a list of dates,tobs,and stations that will be appended with dictionary values for date, tobs, and station number queried above
+    # Create a list of dates,tobs,and stations with dictionary values queried above
     active_station_values = []
     for date, tobs, station in active_station_measurements:
         dates_tobs_dict = {}
@@ -148,13 +140,13 @@ def tobs():
         
     return jsonify(active_station_values) 
 
-# Create a route that when given the start date 
+# Create start date route
 @app.route("/api/v1.0/<start>")
 
 def start_date(start):
     session = Session(engine) 
 
-    # Create query for minimum, average, and max tobs 
+    # Find minimum, average, and maxaimum temperature observations
     start_date_tobs_results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).all()
     
@@ -171,14 +163,12 @@ def start_date(start):
     
     return jsonify(start_date_tobs_values)
 
-# Create a route that when given the start date only, returns the minimum, average, and maximum temperature observed for all dates greater than or equal to the start date entered by a user
+# Create start/end date route
 @app.route("/api/v1.0/<start>/<end>")
 
-# Define function, set start and end dates entered by user as parameters for start_end_date decorator
 def Start_end_date(start, end):
-    session = Session(engine)
-       
-    # Create query for minimum, average, and max tobs where query date is greater than or equal to the start date and less than or equal to end date user submits in URL
+    session = Session(engine)     
+  
 
     start_end_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).\
